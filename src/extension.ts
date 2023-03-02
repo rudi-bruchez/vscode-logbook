e7305995e4397b2924ea63ce232527e2634c07ea
Rudi Bruchez <rudi@babaluga.com>
2023-03-02 14:07:15 +0100

    first commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,39 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('logbook.createMarkdownFile', () => {
         const currentDate = new Date().toISOString().slice(0, 10);
-        const fileName = `${currentDate}.md`;
+        const fileName = `${currentDate}.md`.replace(/-/g, ".");
+
+        const systemLocale = vscode.env.language;
+        const formattedDate = new Date(currentDate).toLocaleDateString(systemLocale, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        });
 
         if (!vscode.workspace.workspaceFolders) {
             vscode.window.showErrorMessage('No workspace folder found. Please open a folder first');
             return;
         }
 
-        const filePath = vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + fileName;
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            vscode.window.showErrorMessage('No active editor found.');
+            return;
+        }
+
+        const activeFolderPath = path.dirname(vscode.Uri.parse(activeEditor.document.uri.toString()).fsPath);
+        
+        const filePath = `${activeFolderPath}/${fileName}`;
+        const content = `# ${formattedDate}\n`;
+
+        // const filePath = vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + fileName;
 
         try {
-            fs.writeFileSync(filePath, '', 'utf-8');
+            fs.writeFileSync(filePath, content, 'utf-8');
             vscode.window.showInformationMessage(`File ${fileName} created successfully.`);
         } catch (err: NodeJS.ErrnoException | unknown) {
             if (err instanceof Error) {
